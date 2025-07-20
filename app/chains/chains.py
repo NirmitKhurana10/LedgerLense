ca0fb0f0a8ee5beab93b4fc1ab36273b00907960
NirmitKhrurana10 <nkhurana1010@gmail.com>
2025-07-19 20:43:56 -0400

    added comments for better explaination

--- app/chains/chains.py
+++ app/chains/chains.py
@@ -32,3 +32,24 @@ def get_chain(retriever):
      | llm
      | StrOutputParser()
 )
+
+
+# ✅ Why Use RunnablePassthrough?
+
+# You need to send both:
+# 	•	context: built by the retriever from the query
+# 	•	question: the raw query text itself
+
+# So instead of splitting it manually, LangChain lets you build a dictionary input pipeline using:
+
+# RunnablePassthrough() just returns the input as-is, and it’s used here to make sure your original question reaches the prompt template unchanged.
+
+# This is a clean, declarative way to say:
+
+# “Use the same input in two places: one goes through the retriever and becomes context, and the other just flows through untouched as question.”
+
+# LangChain internally maps this like:
+
+# Key      :     Value
+# contex   :     The result of running the query through retriever_flat + format_docs
+# question :     The raw string you passed: "What were Amazon's total assets in 2023?"
